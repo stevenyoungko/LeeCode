1028a1a2d4c57afd3d67b17bdc6d23b4ddb12a55
Steven Ko <steven.ko@shoplineapp.com>
2023-06-02 17:43:07 +0800

    feat(binary-search): add missing_number

---
+++ src/binary-search/missing_number.ts
@@ -0,0 +1,23 @@
+function missingNumber(numbs: number[]): number {
+  for(let i = 0; i <= numbs.length; i++) {
+    if (!numbs.includes(i)) {
+      return i
+    } 
+  }
+  return numbs.length
+};
+
+function missingNumber2(numbs: number[]): number {
+  const sortNumbs = numbs.sort((a, b) => a - b)
+  let left = 0;
+  let right = sortNumbs.length
+  while(right > left) {
+    const mid = Math.floor((left + right) / 2)
+    if (sortNumbs[mid] > mid) {
+      right = mid
+    } else {
+      left = mid + 1
+    }
+  }
+  return left
+};
