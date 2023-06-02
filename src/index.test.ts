9a15e814397722373e1a3b5ad9572857b79e7823
Steven Ko <steven.ko@shoplineapp.com>
2023-06-02 17:49:04 +0800

    format

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,8 @@
 // index.test.ts
-import { add } from './index';
+import { add } from "./index";
 
-describe('add function', () => {
-  test('should return the sum of two numbers', () => {
+describe("add function", () => {
+  test("should return the sum of two numbers", () => {
     const result = add(2, 3);
     expect(result).toBe(5);
   });
