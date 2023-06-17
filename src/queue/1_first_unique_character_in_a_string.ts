96bf07032a7fc232a60e5b68857dc90644a8dbc3
Steven Ko <steven.ko@shoplineapp.com>
2023-06-17 18:01:06 +0800

    feat(queue): add 1_first_unique_character_in_a_string

--- src/queue/1_first_unique_character_in_a_string.ts
+++ src/queue/1_first_unique_character_in_a_string.ts
@@ -1,13 +1,13 @@
 function firstUniqChar(s: string): number {
-  const strMap: {[key: string]: number} = {}
-  const strArr = s.split('')
-  for(const s of strArr) {
-      strMap[s] = strMap[s] ? strMap[s] + 1 : 1
+  const strMap: { [key: string]: number } = {};
+  const strArr = s.split("");
+  for (const s of strArr) {
+    strMap[s] = strMap[s] ? strMap[s] + 1 : 1;
   }
   for (const item in strMap) {
-      if (strMap[item] === 1) {
-          return strArr.findIndex(e => e === item)
-      }
+    if (strMap[item] === 1) {
+      return strArr.findIndex((e) => e === item);
+    }
   }
-  return -1
-};
+  return -1;
+}
